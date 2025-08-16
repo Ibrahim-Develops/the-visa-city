bec1dd8366ff4f9c8e68633057460762450761bf
Ibrahim Develops <ibrahimdevelopss@gmail.com>
2025-08-16 19:22:29 +0500

    cors update

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -16,20 +16,23 @@ async function bootstrap() {
     'https://the-visa-city-i5o6juesv-muhammad-ibrahims-projects-834581e1.vercel.app',
     'https://thevisacity.com',
     'https://www.thevisacity.com',
-    'https://api.thevisacity.com',
+    'https://www.thevisacity.com/contact/add',
+    'https://api.thevisacity.com',  
   ];
 
   app.enableCors({
-    origin: [
-      'https://thevisacity.com',
-      'https://www.thevisacity.com',
-      'https://api.thevisacity.com',
-      'https://the-visa-city-i5o6juesv-muhammad-ibrahims-projects-834581e1.vercel.app'
-    ],
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
+
 
   await app.listen(3000, '0.0.0.0');
 
