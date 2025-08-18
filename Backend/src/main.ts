cf907d5e517577d4b8613a5c2aafa026722cfa8d
Ibrahim Develops <ibrahimdevelopss@gmail.com>
2025-08-18 12:45:09 +0500

    cors update

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -1,5 +1,4 @@
 import * as crypto from 'crypto';
-
 if (!globalThis.crypto) {
   (globalThis as any).crypto = crypto;
 }
@@ -20,30 +19,16 @@ async function bootstrap() {
     'https://the-visa-city-i5o6juesv-muhammad-ibrahims-projects-834581e1.vercel.app',
   ];
 
-  const allowedHeaders = [
-    'Origin',
-    'X-Requested-With',
-    'Content-Type',
-    'Accept',
-    'Authentication',
-    'Access-Control-Allow-Credentials',
-    'Access-Control-Allow-Headers',
-    'Access-Control-Allow-Methods',
-    'Access-Control-Allow-Origin',
-    'User-Agent',
-    'Referer',
-    'Accept-Encoding',
-    'Accept-Language',
-    'Access-Control-Request-Headers',
-    'Cache-Control',
-    'Pragma',
-    'Authorization'
-  ];
-
   app.enableCors({
     origin: allowedOrigins,
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'UPDATE', 'OPTIONS'],
-    allowedHeaders: allowedHeaders,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'Authorization',
+    ],
     credentials: true,
   });
 
