fe5a5ded7b57950d8e005fd2fadd3a63c3218c8a
Ibrahim Develops <ibrahimdevelopss@gmail.com>
2025-08-18 14:09:32 +0500

    cors

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -15,10 +15,7 @@ async function bootstrap() {
     origin: '*',
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: [
-      'Origin',
-      'X-Requested-With',
       'Content-Type',
-      'Accept',
       'Authorization',
     ],
     credentials: true,
