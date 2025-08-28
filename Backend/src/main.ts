d3b41fe11a64ac42db932aef1cded0718d6198cd
Ibrahim Develops <ibrahimdevelopss@gmail.com>
2025-08-28 19:23:43 +0500

    backend fix

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -18,7 +18,6 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(3000)
-  // await app.listen(3000, '0.0.0.0');
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
